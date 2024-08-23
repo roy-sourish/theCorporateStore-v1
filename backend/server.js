263a6932caff369164ba68d7dd9e07b774a3240c
Sourish Roy <sourish.roy1973@gmail.com>
2024-08-23 07:46:20 +0000

    setup multer and upload routes

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import bodyParser from "body-parser";
@@ -9,6 +10,7 @@ import connectDB from "./config/db.js";
 import productRoutes from "./routes/productRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import orderRoutes from "./routes/orderRoutes.js";
+import uploadRoutes from "./routes/uploadRoutes.js";
 
 dotenv.config();
 const port = process.env.VITE_PORT || 5000;
@@ -30,6 +32,11 @@ app.get("/", (req, res) => {
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/orders", orderRoutes);
+app.use("/api/upload", uploadRoutes);
+
+// set the dir to the current directory
+const __dirname = path.resolve();
+app.use("/uploads", express.satatic(path.join(__dirname, "/uploads")));
 
 app.use(notFound);
 app.use(errorHandler);
--- frontend/src/pages/admin/ProductEditPage.jsx
+++ frontend/src/pages/admin/ProductEditPage.jsx
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from "react";
-import { useNavigate, useParams } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { toast } from "react-toastify";
+
 import {
   useGetProductDetailsQuery,
   useUpdateProductMutation,
 } from "../../slices/productsApiSlice";
-import { useDispatch } from "react-redux";
 
 function ProductEditPage() {
   const navigate = useNavigate();
@@ -43,78 +45,130 @@ function ProductEditPage() {
     }
   }, [product]);
 
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    const updatedProduct = {
+      productId,
+      name,
+      price,
+      image,
+      brand,
+      category,
+      countInStock,
+      description,
+    };
+    const result = await updateProduct(updatedProduct);
+    if (result.err) {
+      toast.error(result.error);
+    } else {
+      toast.success("Product Updated");
+      navigate("/admin/productlist");
+    }
+  };
+
   return (
-    <form className="max-w-md md:max-w-xl mx-auto mt-5 mb-20">
-      <h1 className="text-3xl my-2 tracking-tight">Edit Product</h1>
-      <label className="form-control w-full">
-        <div className="label">
-          <span className="label-text">Name</span>
+    <div>
+      <form
+        className="max-w-md md:max-w-xl mx-auto mt-5 mb-20"
+        onSubmit={submitHandler}
+      >
+        <div className="breadcrumbs text-sm ">
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/admin/productlist">Manage Products</Link>
+            </li>
+            <li>Edit {name}</li>
+          </ul>
         </div>
-        <input
-          type="text"
-          placeholder="Type here"
-          value={name}
-          className="input input-bordered w-full  rounded"
-        />
-      </label>
-      <label className="form-control w-full ">
-        <div className="label">
-          <span className="label-text">Price</span>
-        </div>
-        <input
-          type="Number"
-          placeholder="Price"
-          value={price}
-          className="input input-bordered w-full  rounded"
-        />
-      </label>
-      <label className="form-control w-full ">
-        <div className="label">
-          <span className="label-text">Brand</span>
-        </div>
-        <input
-          type="text"
-          placeholder="Brand"
-          value={brand}
-          className="input input-bordered w-full  rounded"
-        />
-      </label>
-      <label className="form-control w-full ">
-        <div className="label">
-          <span className="label-text">Count In Stock</span>
-        </div>
-        <input
-          type="Number"
-          placeholder="Count In Stock"
-          value={countInStock}
-          className="input input-bordered w-full  rounded"
-        />
-      </label>
-      <label className="form-control w-full ">
-        <div className="label">
-          <span className="label-text">Category</span>
-        </div>
-        <input
-          type="text"
-          placeholder="Category"
-          value={category}
-          className="input input-bordered w-full rounded"
-        />
-      </label>
-      <label className="form-control w-full ">
-        <div className="label">
-          <span className="label-text">Description</span>
-        </div>
-        <textarea
-          placeholder="Description"
-          value={description}
-          className="textarea textarea-bordered textarea-md w-full rounded"
-        ></textarea>
-      </label>
-      <button type="submit" className="btn btn-wide my-5">
-        Submit
-      </button>
-    </form>
+        <h1 className="text-3xl my-2 tracking-tight">Edit Product</h1>
+        <label className="form-control w-full">
+          <div className="label">
+            <span className="label-text">Name</span>
+          </div>
+          <input
+            type="text"
+            placeholder="Type here"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            className="input input-bordered w-full rounded"
+          />
+        </label>
+        <label className="form-control w-full ">
+          <div className="label">
+            <span className="label-text">Price</span>
+          </div>
+          <input
+            type="Number"
+            placeholder="Price"
+            value={price}
+            onChange={(e) => setPrice(e.target.value)}
+            className="input input-bordered w-full rounded"
+          />
+        </label>
+        <label className="form-control w-full">
+          <div className="label">
+            <span className="label-text">Image</span>
+          </div>
+          <input
+            type="file"
+            className="file-input file-input-bordered w-full rounded"
+          />
+        </label>
+        <label className="form-control w-full ">
+          <div className="label">
+            <span className="label-text">Brand</span>
+          </div>
+          <input
+            type="text"
+            placeholder="Brand"
+            value={brand}
+            onChange={(e) => setBrand(e.target.value)}
+            className="input input-bordered w-full  rounded"
+          />
+        </label>
+        <label className="form-control w-full ">
+          <div className="label">
+            <span className="label-text">Count In Stock</span>
+          </div>
+          <input
+            type="Number"
+            placeholder="Count In Stock"
+            value={countInStock}
+            onChange={(e) => setCountInStock(Number(e.target.value))}
+            className="input input-bordered w-full  rounded"
+          />
+        </label>
+        <label className="form-control w-full ">
+          <div className="label">
+            <span className="label-text">Category</span>
+          </div>
+          <input
+            type="text"
+            placeholder="Category"
+            value={category}
+            onChange={(e) => setCategory(e.target.value)}
+            className="input input-bordered w-full rounded"
+          />
+        </label>
+        <label className="form-control w-full ">
+          <div className="label">
+            <span className="label-text">Description</span>
+          </div>
+          <textarea
+            placeholder="Description"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+            className="textarea textarea-bordered textarea-md w-full rounded"
+          ></textarea>
+        </label>
+        <button type="submit" className="btn btn-wide my-5">
+          Submit
+        </button>
+      </form>
+    </div>
   );
 }
 
